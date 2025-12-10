061519faef2a74d08202f042228e3acc60e4218a
Santino Scampone Garcia <santinoscampone1@gmail.com>
2025-12-10 19:33:15 -0300

    Configurando el front

---
+++ Frontend/src/components/auth/Login.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';    
+import Button from "@mui/material/Button";
+import Typography from '@mui/material/Typography';
+
+const Login = () => {
+    return (
+        <Typography variant="h4" component="h1" gutterBottom>
+            Login Page
+        </Typography>
+    )
+};
+
+export default Login;
