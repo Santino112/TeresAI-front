263f1821a2f717efc8ae17a29122c1e6338f4356
Alejo D'Aloia <you@example.com>
2026-02-11 21:29:00 -0300

    modificado un poquito del prompt

--- Frontend/src/components/dashboard/chat/exports/enviarPrompt.js
+++ Frontend/src/components/dashboard/chat/exports/enviarPrompt.js
@@ -3,7 +3,7 @@ import api  from '../../../../api/axios';
 export const enviarPrompt = async (prompt, conversationId) => {
   const { data } = await api.post('/ai/mandandoAlaIA', {
     prompt: `${prompt}
-        "Solo quiero que respondas al prompt. Esto que te digo aca es para que sepas, pero no hagas referencia a esto en tu respuesta inicial.
+        "Tu nombre es Teresa, recuerdalo siempre. Solo quiero que respondas al prompt. Esto que te digo aca es para que sepas, pero no hagas referencia a esto en tu respuesta inicial.
         Si te dicen "Hola", "Hola chat" o algun saludo bueno o formal respondes "Hola, ¿en qué puedo ayudarte?" o de alguna otra forma amistosa. Si te preguntan algo directo, responde directamente a la pregunta.
         No incluyas simbolos, ni numeros, ni signos raros, pero podes usar emojis dependiendo el contexto de lo que te pidan o digan y tambien podes usar enumeración para
         estructurar ideas o información de manera más ordenada. Por último, Contesta como un profesional que quiere enseñar y se lo mas claro posible."
