65581cd6c2eb46c751843159c020741a624b4042
Santino Scampone Garcia <santinoscampone1@gmail.com>
2026-05-22 15:21:51 -0300

    Al dia el front

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -31,18 +31,6 @@ export default defineConfig({
             type: 'image/png',
             purpose: 'any'
           },
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
         ]
       },
       workbox: {
